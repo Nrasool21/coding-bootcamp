12e3dc0eba49c7093218e842964c5222830a8939
Nazia R <nazia.huma@gmail.com>
2021-03-14 22:29:18 +0000

    added more Js files

--- week-03/day-01/01-Ins_Script-ConsoleLog/script.js
+++ week-03/day-01/01-Ins_Script-ConsoleLog/script.js
@@ -1,2 +1,21 @@
 // External files make your code organized and easier to maintain
 console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
+
+//switch is same as writing a bunch of if statement 
+
+//loop - telling js to it several times
+
+//while loop - syntax is a bit different
+
+//constructor function to make your own object
+
+const promptInput = prompt("enter your number");
+
+function person (name, age) {
+
+
+}
+
+
+/* convert string to number: const number = parseInt(promptInput, 10); 
+if(Number.isNan(number)){}
